c7b832eb2669bdcbc6453c475b533dc57cfa5033
sireesha7412 <saisireesha77777@gmail.com>
2024-07-08 21:54:22 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -27,19 +27,22 @@ void padWithZeros(char* soundex, int sIndex) {
     soundex[SOUNDEX_CODE_LENGTH] = '\0';
 }
 
+void processName(const char* name, char* soundex, int* sIndex) {
+    for (int i = 1; name[i] != '\0' && *sIndex < SOUNDEX_CODE_LENGTH; i++) {
+        char code = getSoundexCode(name[i]);
+        appendCode(soundex, code, sIndex);
+    }
+}
+
 void generateSoundex(const char* name, char* soundex) {
-    if (name == NULL || soundex == NULL || strlen(name) == 0) {
+    if (name == NULL || soundex == NULL || name[0] == '\0') {
         return;
     }
 
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
 
-    for (int i = 1; name[i] != '\0' && sIndex < SOUNDEX_CODE_LENGTH; i++) {
-        char code = getSoundexCode(name[i]);
-        appendCode(soundex, code, &sIndex);
-    }
-
+    processName(name, soundex, &sIndex);
     padWithZeros(soundex, sIndex);
 }
 
